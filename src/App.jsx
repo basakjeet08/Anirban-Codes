99910d4318bcfcad9df431efbde2e3cb07b43a1b
Anirban Basak <jeetbasak2002@gmail.com>
2025-01-03 13:56:48 +0530

    refactor - refactored CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold">Anirban Codes</h1>;
+  return (
+    <div className="h-screen w-screen bg-background flex flex-col">
+      <h1 className="text-3xl font-bold text-onBackground">Anirban Codes</h1>
+    </div>
+  );
 }
 
 export default App;
