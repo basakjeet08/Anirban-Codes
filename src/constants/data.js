b0beb165269e4f4e1676c371ce936bd92531e752
Anirban Basak <jeetbasak2002@gmail.com>
2025-01-03 21:38:10 +0530

    feat - Added details and data about the portfolio

---
+++ src/constants/data.js
@@ -0,0 +1,87 @@
+export const creator = {
+  name: "Anirban Basak",
+  info: `
+        I am a versatile developer skilled in Android development and backend systems with Spring Boot. Currently, I'm expanding into front-end web development to become a full-stack developer.
+
+        Passionate about creating seamless user experiences and robust applications, I thrive on solving challenges and staying updated with the latest technologies.
+    `,
+};
+
+export const experiences = [
+  {
+    company: "MBITS Innovation Private Limited",
+    timeline: "June 2023 - Dec 2023",
+    jobTitle: "Android Developer Intern",
+    location: "Remote",
+    description: `
+        As an Android intern at MBITS, I contributed to the development of Android apps, focusing on feature implementation and optimization. I gained hands-on experience with Kotlin, Java, and Android best practices, strengthening my mobile development skills.
+    `,
+  },
+
+  {
+    company: "IoT Lab, KIIT",
+    timeline: "Sep 2022 - Aug 2024",
+    jobTitle: "Android Lead",
+    location: "DL - 007, KIIT",
+    description: `
+        At IoT Lab KIIT, I started as an Android Developer and was promoted to Android Developer Lead after a year. I led a team of 10+ members, overseeing technical projects and guiding the development of Android-based IoT solutions.
+    `,
+  },
+];
+
+export const projects = [
+  {
+    title: "JetChart",
+    stack: ["Android", "Jetpack Compose", "Data Visualization"],
+    githubLink: "https://github.com/basakjeet08/JetChart",
+    description: `
+        JetChart is a Compose library featuring over 15 unique chart types. Built with SOLID principles and the strategy design pattern, it ensures code reusability and scalability. It utilizes the Java Canva API and Kotlin DrawScopes for custom drawing.
+    `,
+    documentationLink: "https://dev-anirban.gitbook.io/jetchart",
+  },
+
+  {
+    title: "Kritique App",
+    stack: ["Android", "Spring Boot", "Published", "Firebase Auth"],
+    githubLink: "https://github.com/iot-lab-kiit/IoT-Teacher-Review-App",
+    description: `
+        Kritique is a platform for faculty and student reviews, enabling users to rate and provide feedback on faculty members. It features a user-friendly interface, review management, and real-time feedback, helping institutions improve teaching quality and student experiences.
+    `,
+  },
+  {
+    title: "Matrix App",
+    stack: ["Android", "Kotlin", "Jetpack Compose"],
+    githubLink: "https://github.com/iot-lab-kiit/Scavenger-Hunt-App",
+    description: `
+        Matrix is a game app developed for the KIIT Fest 7.0 treasure hunt. I led an 8-member team across Android and Backend, ensuring timely delivery. I designed networking, a unified design system, and a QR scanner module, boosting efficiency. Thorough testing and debugging minimized bugs, ensuring a smooth experience during the event.
+    `,
+  },
+];
+
+export const socials = [
+  {
+    type: "LinkedIn",
+    url: "https://www.linkedin.com/in/anirban-basak-b96055249/",
+  },
+  {
+    type: "GitHub",
+    url: "https://github.com/basakjeet08",
+  },
+  {
+    type: "Instagram",
+    url: "https://www.instagram.com/anirban_0_0",
+  },
+  {
+    type: "Email",
+    url: "mailto:jeetbasak2002@gmail.com",
+  },
+  {
+    type: "Leetcode",
+    url: "https://leetcode.com/u/anirban_0_0/",
+  },
+];
+
+export const contacts = {
+  phone: "+91 7478192996",
+  email: "jeetbasak2002@gmail.com",
+};
