5f8aa291aac9c76ee72c6daf7f6760796fb7fff2
Anirban Basak <jeetbasak2002@gmail.com>
2025-01-05 19:52:32 +0530

    fix - Technology and social animation issues.

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { technologyData } from "../constants/data";
 import { bodyStyles, subHeadingStyles } from "../styles";
-import { motion } from "framer-motion";
 import { scaleIn } from "../animations/variant";
+import { motion } from "framer-motion";
+import { horizontalSlide } from "../animations/variant";
 
 const Technologies = () => {
   // UI Data for Technologies
@@ -11,7 +12,16 @@ const Technologies = () => {
   return (
     <div className="flex flex-col gap-4 pt-6">
       <h1 className={subHeadingStyles}>{sectionHeading}</h1>
-      <p className={bodyStyles}>{bodyText}</p>
+      <motion.div
+        className="overflow-hidden"
+        variants={horizontalSlide("right")}
+        initial="initial"
+        whileInView="final"
+        viewport={{ once: true }}
+      >
+        <p className={bodyStyles}>{bodyText}</p>
+      </motion.div>
+
       <div className="flex flex-wrap gap-4">
         {techList.map((tech, index) => (
           <motion.div
